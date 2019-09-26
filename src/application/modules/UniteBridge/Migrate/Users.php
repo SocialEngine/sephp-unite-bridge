0fc29ae62dfe197124b33386fe531b6b18cc4fbc
RaymondBenc <me@raybenc.com>
2019-09-26 14:57:04 +0200

    Fix user migration issues.

--- src/application/modules/UniteBridge/Migrate/Users.php
+++ src/application/modules/UniteBridge/Migrate/Users.php
@@ -2,17 +2,9 @@
 class UniteBridge_Migrate_Users extends UniteBridge_Migrate_Base {
     protected $table = 'engine4_users';
 
-    protected $map = array(
-        'user_id' => 'id',
-        'email' => 'email',
-        'username' => 'username',
-        'displayname' => 'name',
-        'photo_storage_path' => 'picture',
-        'password' => 'password',
-        'salt' => 'password_salt',
-        'groups' => 'groups',
-        'photo_id' => 'photo_id'
-    );
+    protected $tableIdKey = 'user_id';
+
+    protected $hasPhoto = true;
 
     protected function query ($query) {
         $query
@@ -21,34 +13,41 @@ class UniteBridge_Migrate_Users extends UniteBridge_Migrate_Base {
             ));
     }
 
-    protected function records ($records) {
-        $response = [];
-        foreach ($records as $record) {
-            if ($record['photo_id']) {
-                $record['picture'] = [];
-                $image = $this->db->select()
-                    ->from('engine4_storage_files')
-                    ->where('file_id = ?', $record['photo_id'])
-                    ->query()
-                    ->fetch();
-                $images = $this->db->select()
-                    ->from('engine4_storage_files')
-                    ->where('parent_file_id = ?', $record['photo_id'])
-                    ->query()
-                    ->fetchAll();
+    protected function record ($record)
+    {
+        $record['picture'] = [];
+        if ($record['photo_id']) {
+            $image = $this->db->select()
+                ->from('engine4_storage_files')
+                ->where('file_id = ?', $record['photo_id'])
+                ->query()
+                ->fetch();
+            $images = $this->db->select()
+                ->from('engine4_storage_files')
+                ->where('parent_file_id = ?', $record['photo_id'])
+                ->query()
+                ->fetchAll();
+            $record['picture'][] = [
+                'type' => 'default',
+                'path' => $image['storage_path']
+            ];
+            foreach ($images as $image) {
                 $record['picture'][] = [
-                    'type' => 'default',
+                    'type' => $image['type'],
                     'path' => $image['storage_path']
                 ];
-                foreach ($images as $image) {
-                    $record['picture'][] = [
-                        'type' => $image['type'],
-                        'path' => $image['storage_path']
-                    ];
-                }
             }
-            $response[] = $record;
         }
-        return $response;
+
+        return [
+            'id' => $record['id'],
+            'email' => $record['email'],
+            'username' => $record['username'],
+            'name' => $record['displayname'],
+            'password' => $record['password'],
+            'password_salt' => $record['salt'],
+            'groups' => $record['groups'],
+            'picture' => $record['picture']
+        ];
     }
 }
